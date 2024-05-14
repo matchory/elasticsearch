da7b8f8f1417856e0c4cc1e29b80ea8fe3331beb
Moritz Friedrich <moritz@matchory.com>
2024-05-14 21:56:20 +0200

    Updated logging driver settings to sensible defaults

--- config/logging.php
+++ config/logging.php
@@ -3,11 +3,26 @@
 declare(strict_types=1);
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Elasticsearch Log Channel
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the log channel for your Elasticsearch client.
+    | This allows you to customize the logging output for Elasticsearch, or
+    | disable it entirely if you don't want any log events at all.
+    | By default, the Elasticsearch channel will log to the same output as
+    | the stack channel, which is usually the application's log file.
+    |
+    */
     'channels' => [
         'elasticsearch' => [
             'driver' => 'stack',
+            'channels' => ['stack'],
             'name' => 'elasticsearch',
-            'level' => 'warning',
+            'ignore_exceptions' => false,
+            'level' => 'info',
         ],
     ],
 ];
